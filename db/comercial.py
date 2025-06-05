d0c23f52bea6f5fc14184c037791a375113b52f6
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-06-05 18:45:42 -0300

    Update comercial.py

--- db/comercial.py
+++ db/comercial.py
@@ -1,6 +1,15 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh15380\viewkind0
-}
+import psycopg2
+import pandas as pd
+import os
+
+def get_chamados_comercial():
+    conn = psycopg2.connect(
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"]
+    )
+    df = pd.read_sql("SELECT * FROM ordens_servico ORDER BY data_abertura DESC", conn)
+    conn.close()
+    return df
